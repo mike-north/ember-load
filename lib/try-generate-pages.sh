6ebbfbd50c26bb1ca2f6761f88f5660a28c805cb
Mike North <michael.l.north@gmail.com>
2015-08-24 23:09:38 -0700

    fix auto-deploy

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 echo "Attempting to generate pages"
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" = "master" && "${EMBER_TRY_SCENARIO}" = "ember-1.13"]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master"]]; then
   ember github-pages:commit --message "New release" --branch gh-pages
   git push deploy gh-pages  2>&1 >/dev/null
 else
