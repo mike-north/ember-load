494d5e7dba4bfae685907a4a95a237b5030208a6
Ilya Radchenko <ilya@burstcreations.com>
2015-10-30 09:39:22 -0400

    Conditionally add legacy views addon
    Only add the addon for apps with Ember 2.0+

--- blueprints/ember-load/index.js
+++ blueprints/ember-load/index.js
@@ -1,8 +1,15 @@
+var VersionChecker = require('ember-cli-version-checker');
+
 module.exports = {
   description: 'install ember-load into a typical project',
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function () {
-    return this.addPackageToProject('ember-legacy-views', '~0.2.0');
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember', 'bower');
+
+    if (dep.satisfies('>= 2.0')) {
+      return this.addPackageToProject('ember-legacy-views', '~0.2.0');
+    }
   }
 };
