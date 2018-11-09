7626105747d4d3fa17b9fa77ad6820216e17ab94
Mike North <michael.l.north@gmail.com>
2018-11-08 22:33:12 -0800

    fix: upgrade ember-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,49 @@
+/* eslint-env node */
 module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:ember-suave/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:ember-suave/recommended'],
   env: {
     browser: true
   },
-  rules: {
-    'ember-suave/no-direct-property-access': 'off',
-    'no-extra-boolean-cast': 0,
-    'no-useless-escape': 0
-  }
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: [
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign(
+        {},
+        require('eslint-plugin-node').configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+        }
+      )
+    }
+  ]
 };
--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-load'
+  name: require('./package').name
 };
