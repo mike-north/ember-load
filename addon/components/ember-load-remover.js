7cf8bd95d326de45b50767bb8935be8637f3d029
Mike North <michael.l.north@gmail.com>
2018-12-14 12:40:21 -0800

    fix: ie11-friendly way of removing the loading indicator
    Fixes #134

--- addon/components/ember-load-remover.js
+++ addon/components/ember-load-remover.js
@@ -20,8 +20,13 @@ export default Component.extend({
       this.get('ember-load-config.loadingIndicatorClass') ||
       'ember-load-indicator';
     const elems = document.querySelectorAll(`.${loadingIndicatorClass}`);
+    /**
+     * Very important to iterate over the NodeList this way,
+     * and remove the DOM elements via removeChild to maintain ie11
+     * compatibility
+     */
     for (let i = 0; i < elems.length; i++) {
-      elems[i].remove();
+      elems[i].parentNode.removeChild(elems[i]);
     }
   }
 });
